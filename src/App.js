3617349ba0da042f66890d406cbb22c200a257d1
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-01 19:01:24 -0300

    Requisito 3: resolvido

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
-      <Form />
+      <>
+        <Form />
+        <Card />
+      </>
     );
   }
 }
